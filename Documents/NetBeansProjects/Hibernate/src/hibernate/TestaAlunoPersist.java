05d3df68fdaae911e7a404e6d86b37f31ddcfa07
aula2 <aula2@LABINF0233>
2019-10-31 22:41:49 -0200

    Enviar

--- Documents/NetBeansProjects/Hibernate/src/hibernate/TestaAlunoPersist.java
+++ Documents/NetBeansProjects/Hibernate/src/hibernate/TestaAlunoPersist.java
@@ -8,10 +8,19 @@ public class TestaAlunoPersist {
         a1.setNome("Matheus");
         a1.setIdade(47);
         a1.setMatricula("1111");
+        
+        Aluno a2 = new Aluno();
+        a2.setNome("Breno");
+        a2.setIdade(31);
+        a2.setMatricula("2222");
+
 
         AlunoPersist ap = new AlunoPersist();
 
-        ap.salvar(a1);
+        //ap.salvar(a1);
+        //ap.salvar(a2);
+        ap.remover(a1);
+        ap.remover(a2);
 
     }
 }
